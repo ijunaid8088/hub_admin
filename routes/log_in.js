6bd557e821e4c7311e5adc2c3d10fa990e1f1a57
Junaid Farooq <junaid@evercam.io>
2016-11-15 23:12:02 +0500

    Added post request for getting a user.

--- routes/log_in.js
+++ routes/log_in.js
@@ -1,11 +1,23 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  console.log(req)
-  email
-  res.send('respond with a resource');
+  var username = req.body.username
+  var formData = {
+    password: req.body.password
+  }
+  request.post({
+    url:'http://localhost:4000/api/users/'+ username +'',
+    formData: formData,
+    followAllRedirects: true,
+    }, function optionalCallback(err, httpResponse, body) {
+    if (err) {
+      return console.error('Api call failed:', err);
+    }
+    res.send(body)
+  });
 });
 
 module.exports = router;
