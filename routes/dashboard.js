94bd432d212f02e104c4908cdbcea7f5893f4b75
Junaid Farooq <junaid@evercam.io>
2016-11-16 22:49:43 +0500

    Added session check in dashboard route.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var session = require('express-session');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'Dashboard' });
+  getSession = req.session;
+  if (getSession.body) {
+    console.log(getSession);
+    res.render('dashboard', { title: 'Dashboard' });
+  }
+  else{
+    res.render('index', { title: "Hub Admin" });
+  }
 });
 
 module.exports = router;
